0e202d27445a9faae0563f050946b64750e156c9
IRXCI <RXC.me@yandex.ru>
2024-10-07 15:01:08 +0300

    1

--- cmd/grpc_server/main.go
+++ cmd/grpc_server/main.go
@@ -11,7 +11,7 @@ import (
 	"google.golang.org/grpc/reflection"
 	"google.golang.org/protobuf/types/known/timestamppb"
 
-	desc "github.com/IRXCI/auth/tree/second/pkg/auth"
+	desc "github.com/IRXCI/auth/pkg/auth"
 	emptypb "google.golang.org/protobuf/types/known/emptypb"
 )
 
@@ -22,7 +22,7 @@ type server struct {
 }
 
 func (s *server) CreateUser(_ context.Context, req *desc.CreateUserRequest) (*desc.CreateUserResponse, error) {
-	log.Printf("User %v get id", req.Name)
+	log.Printf("CreateUser is working...")
 
 	return &desc.CreateUserResponse{
 		Id: gofakeit.Int64(),
@@ -33,10 +33,11 @@ func (s *server) GetUser(_ context.Context, req *desc.GetUserRequest) (*desc.Get
 	log.Printf("Note id: %d", req.GetId())
 
 	return &desc.GetUserResponse{
-		Id:    req.GetId(),
-		Name:  gofakeit.Name(),
-		Email: gofakeit.Email(),
-		Role:  desc.Role_USER,
+		Id: req.GetId(),
+		UserAuth: &desc.User{
+			Name:  gofakeit.Name(),
+			Email: gofakeit.Email(),
+			Role:  desc.Role_USER},
 
 		CreatedAt: timestamppb.New(gofakeit.Date()),
 		UpdatedAt: timestamppb.New(gofakeit.Date()),
